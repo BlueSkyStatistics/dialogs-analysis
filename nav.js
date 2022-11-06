71374236ba8784546edde493100a12bdc2ad7fc5
Aaron Rangel <aaron5025@hotmail.com>
2022-11-06 02:20:09 -0600

    Partial correlations

--- nav.js
+++ nav.js
@@ -8,7 +8,9 @@ const nav = {
             "children": [
                 "./correlationTestMultivariable",
                 "./polychorserial",
-                "./correlationTestMultivariableLegacy"
+                "./correlationTestMultivariableLegacy",
+                "./partialCorrelations"
+
             ]
         },
         "./crossTabMultiWay",
