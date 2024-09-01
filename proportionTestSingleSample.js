99299f0e7f2817812e3ecaa20fb2b48137b5ce4c
adpgithub <anildabralpro@gmail.com>
2024-09-01 13:38:43 +0530

    dialog updated

--- proportionTestSingleSample.js
+++ proportionTestSingleSample.js
@@ -11,6 +11,11 @@ var localization = {
         txtbox1: "Confidence level",
         txtbox2: "Null hypothesis (mu in range 0-1)",
         chkbox1: "With continuity correction",
+        dataInCols: "One or more variables contain samples",
+        summarized: "Summarized data",
+        noOfEvents:"No of events",
+        noOfTrials:"No of trials",
+        
         help: {
             title: "Single Sample Proportion Test",
             r_help: "help(prop.test, package=stats)",
@@ -84,26 +89,95 @@ class proportionTestSingleSample extends baseModal {
             label: localization.en.title,
             modalType: "two",
             RCode: `
-BSky_Factor_Variable_Count_Table = xtabs( ~{{selected.tvarbox1 | safe}} , data= {{dataset.name}})
-BSkyFormat(BSky_Factor_Variable_Count_Table,singleTableOutputHeader="Counts")
+{{if (options.selected.gpbox1 =="dataInCols")}}
+{{if (options.selected.tvarbox1.length ==0)}}
+cat("Error: You need to select one or more variables to compute the proportion test or select the option for summarized data")
+{{#else}}
+{{each(options.selected.tvarbox1)}}
+BSky_Factor_Variable_Count_Table = xtabs( ~{{@this}} , data= {{dataset.name}})
+BSkyFormat(BSky_Factor_Variable_Count_Table,singleTableOutputHeader="Counts for variable: {{@this}}")
 BSky_Single_Sample_Proportion_Test = prop.test( rbind(BSky_Factor_Variable_Count_Table), alternative='{{selected.gpbox2 | safe}}', p={{selected.txtbox2 | safe}}, conf.level={{selected.txtbox1 | safe}}, correct={{selected.chkbox1 | safe}})
-BSkyFormat(BSky_Single_Sample_Proportion_Test)
-#remove(BSky_Factor_Variable_Count_Table)
-#remove(BSky_Single_Sample_Proportion_Test)
+BSkyFormat(BSky_Single_Sample_Proportion_Test,  outputTableRenames = c("1-sample proportional test for variable: {{@this}}", ".", "."), repeatAllTableFooter ="hypothesized proportion={{selected.txtbox2 | safe}}, CI={{selected.txtbox1 | safe}}, continuity correction={{selected.chkbox1 | safe}}")
+if (exists('BSky_Factor_Variable_Count_Table')){rm(BSky_Factor_Variable_Count_Table)}
+if (exists('BSky_Single_Sample_Proportion_Test')){rm(BSky_Single_Sample_Proportion_Test)}
+{{/each}} 
+{{/if}}
+{{#else}}
+BSky_Single_Sample_Proportion_Test = prop.test( x ={{selected.noOfEvents | safe}}, n ={{selected.noOfTrials | safe}} , alternative='{{selected.gpbox2 | safe}}', p={{selected.txtbox2 | safe}}, conf.level={{selected.txtbox1 | safe}}, correct={{selected.chkbox1 | safe}})
+BSkyFormat(BSky_Single_Sample_Proportion_Test,  outputTableRenames = c("1-sample proportional test: x={{selected.noOfEvents | safe}}, n={{selected.noOfTrials | safe}}", ".", "."), repeatAllTableFooter ="hypothesized proportion={{selected.txtbox2 | safe}}, CI={{selected.txtbox1 | safe}}, continuity correction={{selected.chkbox1 | safe}}")
+if (exists('BSky_Factor_Variable_Count_Table')){rm(BSky_Factor_Variable_Count_Table)}
+if (exists('BSky_Single_Sample_Proportion_Test')){rm(BSky_Single_Sample_Proportion_Test)}
+{{/if}}
 `
         }
         var objects = {
             content_var: { el: new srcVariableList(config, {action: "move"}) },
+
+            dataInCols: {
+                el: new radioButton(config, {
+                  label: localization.en.dataInCols,
+                  no: "gpbox1",
+                  increment: "dataInCols",
+                  value: "dataInCols",
+                  state: "checked",
+                  extraction: "ValueAsIs"
+                })
+              },
+              
+              summarized: {
+                el: new radioButton(config, {
+                  label: localization.en.summarized,
+                  no: "gpbox1",
+                  increment: "summarized",
+                  dependant_objects: ["noOfEvents", "noOfTrials"],
+                  value: "summarized",
+                  required: true,
+                  state: "",
+                  extraction: "ValueAsIs"
+                })
+              },
+
             tvarbox1: {
-                el: new dstVariable(config, {
+                el: new dstVariableList(config, {
                     label: localization.en.tvarbox1,
                     no: "tvarbox1",
                     filter: "Numeric|Logical|Ordinal|Nominal|Scale",
                     extraction: "NoPrefix|UseComma",
-                    required: true,
+                  
                 })
             },
-            label1: { el: new labelVar(config, { label: localization.en.label1, h: 6 }) },
+
+            noOfEvents: {
+                el: new input(config, {
+                    no: 'noOfEvents',
+                    label: localization.en.noOfEvents   ,
+                    placeholder: "",
+                    allow_spaces:true,
+                    enabled:false,
+                    width: "w-25",
+                    ml:4,
+                    type: "numeric",
+                    extraction: "TextAsIs",
+                    value: "",
+                })
+            },
+            noOfTrials: {
+                el: new input(config, {
+                    no: 'noOfTrials',
+                    label: localization.en.noOfTrials   ,
+                    placeholder: "",
+                    ml:4,
+                    enabled:false,
+                    allow_spaces:true,
+                    width: "w-25",
+                    type: "numeric",
+                    extraction: "TextAsIs",
+                    value: "",
+                })
+            },
+
+
+            label1: { el: new labelVar(config, { label: localization.en.label1, style: "mt-3",h: 6 }) },
             test1: {
                 el: new radioButton(config, {
                     label: localization.en.test1,
@@ -160,7 +234,7 @@ BSkyFormat(BSky_Single_Sample_Proportion_Test)
         }
         const content = {
             left: [objects.content_var.el.content],
-            right: [objects.tvarbox1.el.content, objects.label1.el.content,
+            right: [objects.dataInCols.el.content, objects.tvarbox1.el.content, objects.summarized.el.content,objects.noOfEvents.el.content,objects.noOfTrials.el.content,objects.label1.el.content,
             objects.test1.el.content, objects.test2.el.content, objects.test3.el.content,
             objects.txtbox1.el.content, objects.txtbox2.el.content, objects.chkbox1.el.content],
             nav: {
@@ -171,6 +245,24 @@ BSkyFormat(BSky_Single_Sample_Proportion_Test)
         }
         super(config, objects, content);
         this.help = localization.en.help;
+    }
+    prepareExecution(instance) {
+        var res = [];
+        
+        var code_vars = {
+            dataset: {
+                name: $(`#${instance.config.id}`).attr('dataset') ? $(`#${instance.config.id}`).attr('dataset') : getActiveDataset()
+            },
+            selected: instance.dialog.extractData()
+        }
+        code_vars.selected.tvarbox1 = code_vars.selected.tvarbox1.split(",");
+        if (code_vars.selected.tvarbox1[0] =="")
+        {
+            code_vars.selected.tvarbox1 =[]
+        }
+        const cmd =  instance.dialog.renderR(code_vars);
+        res.push({ cmd: cmd, cgid: newCommandGroup() })
+        return res;
     }
 }
 module.exports.item = new proportionTestSingleSample().render()
