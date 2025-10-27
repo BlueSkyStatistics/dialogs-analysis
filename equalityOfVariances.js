bd3e25e5d09f6be9166ebc0ff7fe0b8211c9bfa5
adpgithub <anildabralpro@gmail.com>
2025-10-27 10:22:01 +0530

    copyright message added on top

--- equalityOfVariances.js
+++ equalityOfVariances.js
@@ -1,4 +1,8 @@
-
+/**
+  * This file is protected by copyright (c) 2023-2025 by BlueSky Statistics, LLC.
+  * All rights reserved. The copy, modification, or distribution of this file is not
+  * allowed without the prior written permission from BlueSky Statistics, LLC.
+ */
 
 class equalityOfVariances extends baseModal {
     static dialogId = 'equalityOfVariances'
