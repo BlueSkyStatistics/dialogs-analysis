ee3f892c69ccd158e5b9d062210027f4a287ebf9
Aaron Rangel <aaron5025@hotmail.com>
2025-11-23 17:45:08 -0600

    Minor changes

--- equalityOfVariances.js
+++ equalityOfVariances.js
@@ -33,19 +33,140 @@ var localization = {
             title: "Tests for Equality of Variances",
             r_help: "help(bartlett.test, package=stats); help(leveneTest, package=car); help(fligner.test, package=stats)",
             body: `
-<b>Description</b><br/>
-Performs tests for the equality of variances across multiple groups.<br/>
-<ul>
-<li><b>Bartlett’s test</b>: Sensitive to departures from normality.</li>
-<li><b>Levene’s test</b>: More robust to non-normality.</li>
-<li><b>Fligner–Killeen test</b>: Non-parametric and very robust.</li>
-</ul>
+<b>Description</b></br>
+Tests equality of variances. Supports data in the following formats<br/>
+wide: Each variable contains measurements e.g. variable 1 contains measurements from machine A, variable 2 contains measurements of machine B...<br/>
+long: The measurements in a variable are grouped by one or more variables e.g. Test scores are grouped by gender and class. <br/>
+P-value adjustment method applied only when the pairwise Levene' test is selected.<br/>
+<b>Usage</b>
+<br/>
+<code> 
+BSky_variance_analysis (<br/>
+    data_format = c("wide" ),<br/>
+    cols_to_compare = c('A','B','C'),<br/>
+    response_col = NULL,<br/>
+    group_col = NULL,<br/>
+    conf_level = 0.95,<br/>
+    alt_hypothesis = "two.sided",<br/>
+    p_adjust_method = "holm",<br/>
+    normal_distribution =FALSE,<br/>
+    Fligner_Killeen =FALSE,<br/>
+  pairwiseComparison=FALSE,<br/>
+    data_name="Dataset1"<br/>
+)<br/>
+BSky_variance_analysis (<br/>
+    data_format = c("long" ),<br/>
+    cols_to_compare = NULL,<br/>
+    response_col = c('Score'),<br/>
+    group_col = c('Gender','Class'),<br/>
+    conf_level = 0.95,<br/>
+    alt_hypothesis = "two.sided",<br/>
+    p_adjust_method = "holm",<br/>
+    normal_distribution =FALSE,<br/>
+     Fligner_Killeen =FALSE,<br/>
+  pairwiseComparison=FALSE,<br/>
+    data_name="Dataset2"<br/>
+)<br/>
+</code> <br/>
 <b>Arguments</b><br/>
-- alternative: two.sided, greater, or less.<br/>
-- conf.level: confidence level for interval estimates.<br/>
-- format: Wide or long data layout.<br/>
-- p.adjust.method: Method for multiple comparison adjustment.
-`
+<ul>
+<li>
+data_format: Can be wide or long
+</li>
+<li>
+cols_to_compare: The variables to compare variances across when dataset is in wide format, NULL for data in long format
+</li>
+<li>
+response_col: The measurement variable to compare variances across groups. NULL for wide data
+</li>
+<li>
+group_col: One or more variables used to group the measurement variable, we test for equality of variances across groups
+</li>
+<li>
+conf_level: Confidence interval
+</li>
+<li>
+alt_hypothesis: Set to two.sided
+</li>
+<li>
+p_adjust_method: Method used to adjust the p value
+</li>
+<li>
+normal_distribution: if TRUE, we assume normality
+</li>
+<li>
+Fligner_Killeen: if TRUE, performs a Fligner_Killeen test
+</li>
+<li>
+pairwiseComparison: Perform a pairwise Levene's test
+</li>
+<li>
+datasetname: Name of the dataset from which x,y and layers (variables) are chosen
+</li>
+</ul>
+<b>Value</b></br>
+Bonferroni confidence intervals for the confidence intervals specified. Test recults for multi-comparison tests (when normality is not assumed) and results of other tests
+<br/>
+<b>Example</b></br>
+<code> 
+BSky_variance_analysis (<br/>
+    data_format = c("long" ),<br/>
+    cols_to_compare = NULL,<br/>
+    response_col = c('Score'),<br/>
+    group_col = c('Gender','Class'),<br/>
+    conf_level = 0.95,<br/>
+    alt_hypothesis = "two.sided",<br/>
+    p_adjust_method = "holm",<br/>
+    normal_distribution =FALSE,<br/>
+     Fligner_Killeen =FALSE,<br/>
+  pairwiseComparison=FALSE,<br/>
+    data_name="Dataset2"<br/>
+)<br/>
+</code> <br/>
+
+<b>Details</b></br>
+Multiple comparisons test</br>
+Used when you do not select “Use test based on normal distribution” – i.e., you favour methods robust to non-normal distributions. </br>
+If you have k > 2 groups, we compute the intervals for standard deviations such that if two intervals do not overlap, the associated standard deviations are 
+significantly different.</br> 
+For k = 2 samples, it uses Bonett’s method for testing two variances with hypothesized ratio = 1. </br>
+When the overall multiple comparison test is significant, the standard deviations that correspond to the non-overlapping intervals are statistically different.</br>
+Advantages: More powerful than Levene’s method in many situations (for continuous distributions). But when sample sizes are small (< 20) or distributions are 
+extremely skewed/heavy-tailed, the type I error rate may be inflated.</br>
+P-value for the test</br>
+If there are 2 samples in the design, then we calculate the p-value for the multiple comparisons test using Bonett's method for a 2 variances test and a hypothesized ratio, Ρo, of 1.</br>
+If there are k > 2 samples in the design, then let Pij be the p-value of the test for any pair (i, j ) of samples. The p-value for the multiple comparisons procedure as an overall test of equality of variances is given by the following:</br>
+P =min{Pij, 1<=i<j<=k}
+</br>
+Levene’s test statistic</br>
+Levene’s test is used to test the null hypothesis that variances are equal across groups (H₀: all variances equal) versus the alternative that at least 
+one variance differs. </br>
+It is more robust to departures from normality compared to tests that assume normality (e.g., Bartlett’s). </br>
+Specifically, we use the Brown-Forsythe modification (absolute deviations from the sample median rather than mean) for improved robustness. </br>
+Interpretation: If the p-value < α, you reject H₀ and conclude variances are not equal. </br>
+</br>
+Bartlett’s test statistic</br>
+Bartlett’s test is designed to test for equality of variances when the data are normally distributed. H₀: all variances equal. When k > 2 groups,
+we use Bartlett’s test (if “use test based on normal distribution” is selected). </br>
+The test statistic (commonly denoted B) is based on comparing the weighted arithmetic mean and the weighted geometric mean of sample variances. </br>
+The statistic follows (approximately) a chi-square distribution with k-1 df. </br>
+Because Bartlett’s test assumes normality, it is sensitive to departures from normality (i.e., non-normal or heavy-tailed distributions can lead to 
+misleading results)</br>
+</br>
+F-test statistic</br>
+When there are exactly two groups (k = 2), we use an F-test rather than Bartlett’s (if “use test based on normal distribution” is selected). </br>
+H₀: the variances are equal (σ₁² = σ₂²). </br>
+The F-statistic is computed as the ratio of the two sample variances (larger over smaller). Then the p-value depends on the alternative hypothesis
+ (one-sided “less than” or “greater than”, or two-sided). </br>
+Interpretation: If p-value < α, reject H₀ and conclude that variances differ. It is valid only under the assumption of normality.</br>
+<b>Value</b><br/>
+
+<b>Package</b></br>
+BlueSky</br>
+<b>Help</b></br>
+Please contact support@blueskystatistics.com for additional information.
+    `
+
         }
     }
 }
@@ -65,7 +186,7 @@ library(ggplot2)
 library(grid)
 
 {{if(options.selected.groupvar =="")}}
-test<-BSky_variance_analysis (
+BSkyEqualityVariances<-BSky_variance_analysis (
     data_format = c("wide" ),
     cols_to_compare = c({{selected.tvarbox1 | safe}}),
     response_col = NULL,
@@ -79,7 +200,7 @@ test<-BSky_variance_analysis (
     data_name="{{dataset.name}}"
 )
 {{#else}}
-test<-BSky_variance_analysis (
+BSkyEqualityVariances<-BSky_variance_analysis (
     data_format = c("long" ),
     cols_to_compare = NULL,
     response_col = c({{selected.tvarbox1 | safe}}),
@@ -93,7 +214,7 @@ test<-BSky_variance_analysis (
     data_name="{{dataset.name}}"
 )
 {{/if}}
-
+if (exists('BSkyEqualityVariances'))rm(BSkyEqualityVariances)
 `
         }
 
